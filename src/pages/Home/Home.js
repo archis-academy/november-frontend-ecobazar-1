9702d8abc2afb67e7159f96702b2676c6defb8d0
bugraozdemir-dev <dev.bugraozdemir@gmail.com>
2025-11-18 21:15:59 +0300

    Category card component implemented

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,13 @@
+import CategoryCard from "@/components/CategoryCard/CategoryCard";
+
+// const data = {
+//   imageSrc: "/images/category-image-1.png",
+//   name: "Fresh Fruit",
+// };
+
 const HomePage = () => {
   const container = document.createElement("div");
+  //   container.appendChild(CategoryCard(data));
 
   return container;
 };
