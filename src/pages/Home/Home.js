1b964cecb735d0525ad5e115a811ab2a1397bc70
bugraozdemir-dev <dev.bugraozdemir@gmail.com>
2025-11-20 12:10:55 +0300

    Comment line removed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,5 @@
-import CategoryCard from "@/components/CategoryCard/CategoryCard";
-
-const data = {
-  imageSrc: "/images/fruits.png",
-  name: "Fresh Fruit",
-};
-
 const HomePage = () => {
   const container = document.createElement("div");
-  container.appendChild(CategoryCard(data));
 
   return container;
 };
