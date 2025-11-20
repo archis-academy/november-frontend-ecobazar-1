6ec52705f8b3c4987a4a1a581dd2c10bb489c0f8
Yusuf Kaya <yusuf.kaya1059@gmail.com>
2025-11-21 00:48:29 +0300

    category section has been created

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,10 @@
+import CategorySection from "@/components/CategoryCard/CategorySection";
+
 const HomePage = () => {
   const container = document.createElement("div");
+
+  // category section has been added to Homepage.
+  container.appendChild(CategorySection());
 
   return container;
 };
--- src/services/services.js
+++ src/services/services.js
@@ -1,1 +1,12 @@
-export const getCategories = async () => {};
+import { API } from "./restConfiguration";
+import axios from "axios";
+
+export const getCategories = async () => {
+    try {
+        const response = await axios.get(`${API}/category`);
+        return response.data;
+    }
+    catch (err) {
+        console.error(err);
+    }
+};
