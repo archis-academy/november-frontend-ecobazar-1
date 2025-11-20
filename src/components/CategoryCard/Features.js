0b788a4537517b648fbce06bd8d7900461a5ee89
Gulizar Korbulak <korbulakgulizar@gmail.com>
2025-11-20 15:15:01 +0100

    [NO:10]-gulizar Imlement-the Features-Section

---
+++ src/components/CategoryCard/Features.js
@@ -0,0 +1,42 @@
+export function renderFeaturesSection() {
+  const section = document.createElement("section");
+  section.className = "features-wrapper";
+  section.innerHTML = `
+    <div class="features-wrapper">
+      <section class="features">
+        <div class="feature-box">
+          <img src="/images/delivery-truck-1.svg" alt="" />
+          <div class="feature-box-container">
+            <h3>Free Shipping</h3>
+            <p>Free shipping on all your order</p>
+          </div>
+        </div>
+
+        <div class="feature-box">
+          <img src="/images/heads.svg" alt="" />
+          <div class="feature-box-container">
+            <h3>Customer Support 24/7</h3>
+            <p>Instant access to Support</p>
+          </div>
+        </div>
+
+        <div class="feature-box">
+          <img src="images/shopping-bag.svg" alt="" />
+          <div class="feature-box-container">
+            <h3>100% Secure Payment</h3>
+            <p>We ensure your money is safe</p>
+          </div>
+        </div>
+
+        <div class="feature-box">
+          <img src="/images/package.svg" alt="" />
+          <div class="feature-box-container">
+            <h3>Money-Back Guarantee</h3>
+            <p>30 Days Money-Back Guarantee</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  `;
+  return section;
+}
--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import HomePage from "@pages/Home/Home";
 import "./main.scss";
 import { getCategories } from "./services/services";
 import { storeData } from "./services/store";
+import { renderFeaturesSection } from "./components/Features.js";
+
+import "./components/features.css";
 
 const app = document.querySelector("#app");
 
 page("/", async () => {
   storeData.category = await getCategories();
+  app.innerHTML = "";
   app.appendChild(HomePage());
+  app.appendChild(renderFeaturesSection());
 });
 
 page();
