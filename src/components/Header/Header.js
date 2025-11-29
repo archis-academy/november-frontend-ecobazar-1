cf5c22affd206968bb1fb984406c163f7f4878e4
aslı doğan <a.dgn2003@gmail.com>
2025-11-30 02:21:35 +0300

    [NO-4] - .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,52 +4,70 @@ import heartIcon from "@images/Header/heart.svg";
 import locationIcon from "@images/Header/location.svg";
 import logoIcon from "@images/Header/logo.svg";
 import searchIcon from "@images/Header/search.svg";
-//import cartIcon from "@images/Header/cart.svg";
+//import sepetIcon from "@images/Header/cart.svg";
 
 const Header = () => {
   const header = document.createElement("header");
   header.classList.add(styles.header);
+header.innerHTML = `
+  <div class="${styles.inner}">
 
-  header.innerHTML = `
-    <div class="${styles.inner}">
+    <!-- TOP BAR -->
+    <div class="${styles.top}">
 
-      <div class="${styles.left}">
-          <div class="${styles.location}">
-             <img src="${locationIcon}" alt="location" class="${styles.icon}" />
-             <span>Store Location: Lincoln - 344, Illinois, Chicago, USA</span>
-          </div>
-
-          <div class="${styles.logo}">
-             <img src="${logoIcon}" alt="logo" class="${styles.logoIcon}" />
-             <span class="${styles.logoText}">Ecobazar</span>
-          </div>
+      <div class="${styles.location}">
+        <img src="${locationIcon}" alt="location" class="${styles.icon}" />
+        <span>Store Location: Lincoln - 344, Illinois, Chicago, USA</span>
       </div>
 
-      <div class="${styles.center}">
-        <div class="${styles.searchBox}">
-           <img src="${searchIcon}" alt="search" class="${styles.searchIcon}" />
-           <input type="text" class="${styles.searchInput}" placeholder="Search" />
-           <button class="${styles.searchBtn}">
-              <img src="${arrowIcon}" alt="submit search" class="${styles.submitIcon}" />
-           </button>
-        </div>
+      <div class="${styles.language}">
+        Eng
+        <img src="${arrowIcon}" alt="arrow" class="${styles.arrowIcon}" />
       </div>
 
-      <div class="${styles.right}">
-          <button class="${styles.iconBtn}">
-            <img src="${heartIcon}" alt="wishlist" class="${styles.icon}" />
-          </button>
-
-          <div class="${styles.cart}">
-              <button class="${styles.cartBtn}">
-                 <img src="${""}" alt="cart" class="${styles.icon}" />
-              </button>
-              <span class="${styles.cartTotal}">$57.00</span>
-          </div>
+      <div class="${styles.currency}">
+        USD
+        <img src="${arrowIcon}" alt="arrow" class="${styles.arrowIcon}" />
       </div>
 
-    </div>
-  `;
+      <div class="${styles.auth}">
+        <a href="#">Sign In</a> / <a href="#">Sign Up</a>
+      </div>
+
+    </div> <!-- /top -->
+
+
+    <!-- BOTTOM BAR -->
+    <div class="${styles.bottom}">
+
+      <div class="${styles.logo}">
+        <img src="${logoIcon}" alt="logo" class="${styles.logoIcon}" />
+        <span class="${styles.logoText}">Ecobazar</span>
+      </div>
+
+      <div class="${styles.searchBox}">
+        <img src="${searchIcon}" alt="search" class="${styles.searchIcon}" />
+        <input type="text" class="${styles.searchInput}" placeholder="Search" />
+        <button class="${styles.searchBtn}">
+          <span>Search</span>
+        </button>
+      </div>
+
+      <button class="${styles.iconBtn}">
+        <img src="${heartIcon}" alt="wishlist" class="${styles.icon}" />
+      </button>
+
+      <div class="${styles.cart}">
+        <button class="${styles.cartBtn}">
+          <img src="${""}" alt="cart" class="${styles.icon}" />
+        </button>
+        <span class="${styles.cartTotal}">$57.00</span>
+      </div>
+
+    </div> <!-- /bottom -->
+
+  </div>
+`;
 
   return header;
 };
